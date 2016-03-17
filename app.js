4202c6a50916f78e2c7d740545aff1144978033c
Brady Brown <bobrown101@gmail.com>
2016-03-17 15:14:39 -0400

    Updated package.json:

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ mongoose.connection.on('error', function() {
 /**
  * Express configuration.
  */
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(compress());
